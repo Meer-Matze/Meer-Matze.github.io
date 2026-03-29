ddc90ae62b64d0e47bea144cf4a8e637440f437e
Meer-Katze <Meer_Katze@outlook.com>
2026-03-29 15:13:13 +0800

    添加了图标，修复了bug

--- src/config.ts
+++ src/config.ts
@@ -30,6 +30,7 @@ export const siteConfig: SiteConfig = {
 		depth: 2, // Maximum heading depth to show in the table, from 1 to 3
 	},
 	favicon: [
+		{ src: "/favicon/my-favicon.png" },
 		// Leave this array empty to use the default favicon
 		// {
 		//   src: '/favicon/icon.png',    // Path of the favicon, relative to the /public directory
